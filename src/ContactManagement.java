bed8762ca2680173c252c32e0e0966957b74239f
Tom <64775524+tom-rh@users.noreply.github.com>
2022-06-13 14:39:45 +0200

    add saveContacts method

--- src/ContactManagement.java
+++ src/ContactManagement.java
@@ -1,3 +1,6 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Vector;
 
 public class ContactManagement {
@@ -12,5 +15,16 @@ public class ContactManagement {
     public void addContact(String prenom, String nom, int age, String telephone) {
         Contact contact = new Contact(prenom, nom, age, telephone);
         contactList.add(contact);
+    }
+
+    public void saveContacts() {
+        PrintWriter out = null;
+        try {
+            out = new PrintWriter(new FileWriter("contacts.txt"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        out.write("test");
+        out.close();
     }
 }
