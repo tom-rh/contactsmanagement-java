3a7b4c2836b8e8283d9d4eb0d68832d877144b05
Tom <64775524+tom-rh@users.noreply.github.com>
2022-06-13 14:27:21 +0200

    add some methods in Contact class

--- src/Contact.java
+++ src/Contact.java
@@ -1,2 +1,44 @@
-package PACKAGE_NAME;public class Contact {
+public class Contact {
+    private String _prenom;
+    private String _nom;
+    private int _age;
+    private String _telephone;
+
+    public Contact() {
+        _prenom = "Inconnu";
+        _nom = "Inconnu";
+        _age = 0;
+        _telephone = "";
+    }
+
+    public Contact(String prenom, String nom, int age, String telephone) {
+        _prenom = prenom;
+        _nom = nom;
+        _age = age;
+        _telephone = telephone;
+    }
+
+    public String getPrenom() {
+        return _prenom;
+    }
+
+    public String getNom() {
+        return _nom;
+    }
+
+    public int getAge() {
+        return _age;
+    }
+
+    public String getTelephone() {
+        return _telephone;
+    }
+
+    public void setPrenom(String prenom) {
+        _prenom = prenom;
+    }
+
+    public void setNom(String nom) {
+        _nom = nom;
+    }
 }
