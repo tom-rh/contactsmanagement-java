0dece38f51681a744b4168bc25e983e5a5206edb
Tom <64775524+tom-rh@users.noreply.github.com>
2022-06-13 14:26:51 +0200

    addContact method

--- src/ContactManagement.java
+++ src/ContactManagement.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class ContactManagement {
+import java.util.Vector;
+
+public class ContactManagement {
+    // tableau de contacts
+    private Vector<Contact> contactList;
+    private int _numberOfContacts;
+
+    public ContactManagement(){
+        _numberOfContacts = 0;
+    }
+
+    public void addContact(String prenom, String nom, int age, String telephone) {
+        Contact contact = new Contact(prenom, nom, age, telephone);
+        contactList.add(contact);
+    }
 }
