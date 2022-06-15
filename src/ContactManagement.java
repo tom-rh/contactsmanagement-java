cda7d72c50025199202dcde3301a3d683e8121a4
Tom <64775524+tom-rh@users.noreply.github.com>
2022-06-15 11:00:38 +0200

    rename variable

--- src/ContactManagement.java
+++ src/ContactManagement.java
@@ -2,11 +2,11 @@ import java.io.*;
 import java.util.ArrayList;
 
 public class ContactManagement {
-    private final ArrayList<Contact> contactList;
+    private final ArrayList<Contact> _contactList;
 
     public ContactManagement(){
-        contactList = new ArrayList<Contact>();
-        // Leture des lignes du fihier de sauvegarde pour initialiser la liste de contacts
+        _contactList = new ArrayList<Contact>();
+        // Lecture des lignes du fichier de sauvegarde pour initialiser la liste de contacts
         try(BufferedReader br = new BufferedReader(new FileReader("contacts.txt"))) {
             for(String line; (line = br.readLine()) != null; ) {
                 String[] parts = line.split(";");
@@ -19,32 +19,32 @@ public class ContactManagement {
     }
 
     public int getNumberOfContacts() {
-        return contactList.size();
+        return _contactList.size();
     }
 
     public void addContact( String prenom, String nom, int age, String telephone) {
         Contact contact = new Contact(prenom, nom, age, telephone);
-        contactList.add(contact);
+        _contactList.add(contact);
     }
     public void addContact(int id, String prenom, String nom, int age, String telephone) {
         Contact contact = new Contact(id, prenom, nom, age, telephone);
-        contactList.add(contact);
+        _contactList.add(contact);
     }
 
     public void saveContacts() {
-        PrintWriter out = null;
+        PrintWriter out;
         try {
             out = new PrintWriter(new FileWriter("contacts.txt"));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
         int count = 0;
-        while (contactList.size() > count) {
-            out.write(String.valueOf(contactList.get(count).getId()) + ";");
-            out.write(contactList.get(count).getPrenom() + ";");
-            out.write(contactList.get(count).getNom() + ";");
-            out.write(String.valueOf(contactList.get(count).getAge()) + ";");
-            out.write(contactList.get(count).getTelephone());
+        while (_contactList.size() > count) {
+            out.write(String.valueOf(_contactList.get(count).getId()) + ";");
+            out.write(_contactList.get(count).getPrenom() + ";");
+            out.write(_contactList.get(count).getNom() + ";");
+            out.write(String.valueOf(_contactList.get(count).getAge()) + ";");
+            out.write(_contactList.get(count).getTelephone());
             out.println();
             count++;
         }
@@ -54,8 +54,8 @@ public class ContactManagement {
     public void displayContactListConsole() {
         int count = 0;
         System.out.println("id ; prenom ; nom ; age ; telephone");
-        while (contactList.size() > count) {
-            System.out.println(String.valueOf(contactList.get(count).getId()) + ";" + contactList.get(count).getPrenom() + ";" + contactList.get(count).getNom() + ";" + String.valueOf(contactList.get(count).getAge()) + ";" + contactList.get(count).getTelephone());
+        while (_contactList.size() > count) {
+            System.out.println(String.valueOf(_contactList.get(count).getId()) + ";" + _contactList.get(count).getPrenom() + ";" + _contactList.get(count).getNom() + ";" + String.valueOf(_contactList.get(count).getAge()) + ";" + _contactList.get(count).getTelephone());
             count++;
         }
     }
